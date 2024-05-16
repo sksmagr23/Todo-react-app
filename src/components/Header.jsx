58291557c028c2787e25c8ec73f7b74be36f30b9
sksmagr23 <sakshamag34@gmail.com>
2024-05-16 08:55:49 +0530

    Add Vite-based React app files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className="text-center w-screen bg-teal-800 font-sans font-semibold py-1 text-white ">
+      TODO | your personal task management app
+    </div>
+  )
+}
+
+export default Header
